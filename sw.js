64b011fdd50f67bd5cc67e1635347dcac8fcabd8
程浩 <chenghao028935@gtjas>
2026-07-02 11:41:56 +0800

    init: 初始化应用

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 const CACHE = 'love-ledger-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/css/app.css',
-  '/js/app.js',
-  '/js/modules/cost-calc.js',
-  '/manifest.json',
-  '/assets/icon-192.png',
-  '/assets/icon-512.png',
-  '/assets/apple-touch-icon.png'
+  './',
+  './index.html',
+  './css/app.css',
+  './js/app.js',
+  './js/modules/cost-calc.js',
+  './manifest.json',
+  './assets/icon-192.png',
+  './assets/icon-512.png',
+  './assets/apple-touch-icon.png'
 ];
 
 self.addEventListener('install', e => {
